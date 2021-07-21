64e090dcfac1a59feef2e95c9a7aeced822bf50b
zhouxi <zhouxi>
2021-07-21 18:32:56 +0800

    init

--- copy.sh
+++ copy.sh
@@ -1,10 +1,8 @@
 export dir=/Volumes/mojave/Users/zed/Java/Project/IdeaWorkSpace/xxl-job/package
 
-#cp /Volumes/mojave/Users/zed/Java/Project/IdeaWorkSpace/xxl-job/xxl-job-core/target/xxl-job-core-2.3.1-SNAPSHOT.jar $dir/xxl-job-core-2.3.1-SNAPSHOT.jar
 cp /Volumes/mojave/Users/zed/Java/Project/IdeaWorkSpace/xxl-job/xxl-job-admin/target/xxl-job-admin-2.3.1-SNAPSHOT.jar $dir/xxl-job-admin-2.3.1-SNAPSHOT.jar
-#cp /Volumes/mojave/Users/zed/Java/Project/IdeaWorkSpace/xxl-job/xxl-job-executor-samples/xxl-job-executor-sample-frameless/target/xxl-job-executor-sample-frameless-2.3.1-SNAPSHOT.jar $dir/xxl-job-executor-sample-frameless-2.3.1-SNAPSHOT.jar
 cp /Volumes/mojave/Users/zed/Java/Project/IdeaWorkSpace/xxl-job/xxl-job-executor-samples/xxl-job-executor-sample-springboot/target/xxl-job-executor-sample-springboot-2.3.1-SNAPSHOT.jar $dir/xxl-job-executor-sample-springboot-2.3.1-SNAPSHOT.jar
-
+cp /Volumes/mojave/Users/zed/Java/Project/IdeaWorkSpace/xxl-job/xxl-job-executor-samples/ssm/target/ssm-0.0.1-SNAPSHOT.jar $dir/ssm-0.0.1-SNAPSHOT.jar
 
 zip -r package.zip package/*.jar
 scp /Volumes/mojave/Users/zed/Java/Project/IdeaWorkSpace/xxl-job/package.zip centos@32a017h342.wicp.vip:/home/app/java/server
